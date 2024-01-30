8a2d929ca2b97dbfdfe87f084fcb52b6806299f2
Annajuuj <canna@acad.ifma.edu.br>
2024-01-30 17:33:30 -0300

    exportamos algo né🤔 algo né

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import {db, firestore} from '../banco_de_dados/firebase';
 
 const app = express();
 
@@ -7,4 +8,5 @@ const app = express();
 app.listen(3000,function(){
     console.log("Serviço rodando em http://localcat:3000");
 });
+
 
