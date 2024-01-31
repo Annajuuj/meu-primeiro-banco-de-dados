eaa7a0ac6202e5ede5940bb57a4b15385994f9a1
Annajuuj <canna@acad.ifma.edu.br>
2024-01-31 17:41:34 -0300

    Hoje vai ser criação de rota

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,26 @@ import {db, firestore} from '../banco_de_dados/firebase';
 
 const app = express();
 
+app.get('/', (req, res) =>{
+    res.send('Ja ta podendo sair da API já?💕');
+});
 
+app.post('/usuario', async (req, res) => {
+    const usuario = req.body.nome
+
+    try {
+        const docRef = await firestore.addDoc(firestore.collection(db, 'usuarios'), {
+            nome: nome,
+        })
+        res.send(docRef.id)
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e)
+    }
+})
 
 app.listen(3000,function(){
-    console.log("Serviço rodando em http://localcat:3000");
+    console.log("Serviço rodando em http://localhost:3000");
 });
 
 
